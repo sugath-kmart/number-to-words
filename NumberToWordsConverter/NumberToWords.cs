c16bc752e7b846418887ba5033e28f61f5c371cc
Sugath <sugathb@gmail.com>
2022-07-31 21:12:45 +1000

    draft-console-app

---
+++ NumberToWordsConverter/NumberToWords.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumberToWordsConverter
+{
+    public class NumberToWords
+    {
+        public static string ConvertToWords(string price)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NumberToWordsConverter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NumberToWordsConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Price to convert to words");
+
+            var convertedPricer = NumberToWords.ConvertToWords(Console.ReadLine());
+
+            Console.WriteLine("Price in words is \n{0}", convertedPricer);
+            Console.ReadKey();
+        }
+    }
+}
